15fb206da480bf9552ff5a87679f9064beebf98b
duongnguyen233 <duongnguyen23031996@gmail.com>
2025-10-17 12:02:40 +1100

    [DuongNT] Change database URL to AWS

--- Back-End/app/core/config.py
+++ Back-End/app/core/config.py
@@ -1,9 +1,17 @@
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+
+# Load .env file when this module is imported
+load_dotenv()
 
 class Settings(BaseSettings):
-  DATABASE_URL: str = "postgresql://caredata_user:123456789@localhost/caredata_db"
+  DATABASE_URL: str
   SECRET_KEY: str = "your_secret_key"
   ALGORITHM: str = "HS256"
   ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+  class Config:
+    env_file = ".env"
+    env_file_encoding = "utf-8"
 
 settings = Settings()
